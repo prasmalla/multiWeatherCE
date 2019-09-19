d7e37d4648a9529efc61f2497f9db6527483c146
prasanna malla <mallaprasanna@gmail.com>
2019-09-18 22:32:02 -0400

    add UI

---
+++ content_script.js
@@ -0,0 +1,11 @@
+function clickPlanet() {
+  // var planets = document.getElementsByClassName("planet-name"),
+  //     randomplanet = Math.floor(Math.random() * planets.length),
+  //     clickplanet = planets[randomplanet];
+
+  // clickplanet.click();
+  //   chrome.tabs.create({ url: "results.html", active: false });
+  document.body.innerHTML = "foo";
+}
+
+clickPlanet();
